769d4ba04beba9642723d5079782f31b06ede396
wangyongliang <viporg@126.com>
2026-06-02 23:42:12 +0200

    Update Package.swift for 1.13.2

--- Package.swift
+++ Package.swift
@@ -20,13 +20,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "onnxruntime",
-            url: "https://github.com/fdddf/sherpa-onnx-spm/releases/download/1.12.23/onnxruntime.xcframework.zip",
-            checksum: "09e7505bd0e4376014fa39838c99f9b38471e992f1edb7886f74e0ba902254bb"
+            url: "https://github.com/fdddf/sherpa-onnx-spm/releases/download/1.13.2/onnxruntime.xcframework.zip",
+            checksum: "f2052998f370ae34e4c1d8427920e88ff1b66eb0cefc93cb26d2d793c7726075"
         ),
         .binaryTarget(
             name: "sherpa_onnx",
-            url: "https://github.com/fdddf/sherpa-onnx-spm/releases/download/1.12.23/sherpa-onnx.xcframework.zip",
-            checksum: "18833536d5eac54806e1c808934a89fc9bb59301e1d5c717daa50580ea5da650"
+            url: "https://github.com/fdddf/sherpa-onnx-spm/releases/download/1.13.2/sherpa-onnx.xcframework.zip",
+            checksum: "f97867ff963ae663599c37b28dfec97d3fb0d5cd9c7fa4bb675aceb0f8d8a135"
         ),
     ]
 )
